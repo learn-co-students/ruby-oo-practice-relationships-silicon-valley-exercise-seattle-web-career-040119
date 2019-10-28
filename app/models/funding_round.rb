29766acaa0d8af9c6a26c95775ea710a776dc5d8
github username <github email address>
2019-10-28 14:49:54 -0700

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,4 +1,21 @@
 class FundingRound
 
+  attr_accessor :type, :investment
+  attr_reader :startup, :venture_capitalist
+
+  @@all = []
+
+  def initialize(type, investment, startup, venture_capitalist)
+    @type = type
+    @investment = investment.to_f
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
  
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,17 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+s1 = Startup.new("Awesome Inc.", "Aaron", "Actuary")
+s2 = Startup.new("Bro Corp", "Brad", "Billing")
+s3 = Startup.new("Cool Corp", "Claire", "Chemistry")
+
+vc1 = VentureCapitalist.new("Xavier", 2000000000)
+vc2 = VentureCapitalist.new("Yvette", 2000000000)
+vc3 = VentureCapitalist.new("Zack", 1000)
+
+fr1 = FundingRound.new("angel", 1000, s1, vc1)
+fr2 = FundingRound.new("pre-seed", 2000, s2, vc2)
+fr3 = FundingRound.new("seed", 3000, s3, vc3)
+
+Pry.start
+puts "done" #leave this here to ensure binding.pry isn't the last line
